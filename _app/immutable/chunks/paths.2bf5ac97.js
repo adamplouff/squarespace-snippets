c41d2980819f70f000e50483dd15810b7072deda
Adam Plouff <adamplouff@gmail.com>
2023-08-07 09:44:09 -0400

    Updates

---
+++ _app/immutable/chunks/paths.2bf5ac97.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_yx50tx)==null?void 0:s.base)??"/squarespace-snippets";var e;const a=((e=globalThis.__sveltekit_yx50tx)==null?void 0:e.assets)??t;export{a,t as b};
